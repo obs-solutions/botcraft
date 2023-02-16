576612f0bcb501ace2cec28668bc580a46e9dfb5
Ammar Al-Loubani <ammar.alloubani@odoo-bs.com>
2023-02-16 16:24:39 +0100

    Revert "[ADD] DIN 5008 Module"
    This reverts commit 5c8989a94bd220fc7f88cff70d65af6b224b4ccc.

--- DIN-5008-15.0-din-report-shj (11)/DIN-5008-15.0-din-report-shj/obs_din_qweb_report/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_config_setting
-from . import res_company
-from . import base_document_layout
-from . import account_move_line
-from . import sale_order_line
-from . import purchase_order_line
-from . import stock_move
