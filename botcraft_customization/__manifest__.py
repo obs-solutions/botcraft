48aec2d38c3429ab9825c839b12fe93bde22c886
RajkotiyaChirag <chirag.rajkotiya@gmail.com>
2023-03-06 15:47:58 +0530

    [ADD] botcraft customization for pricelist.

---
+++ botcraft_customization/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'BotCraft Subscription PriceList',
+    'version': '16.0.0.1',
+    'summary': 'BotCraft Subscription PriceList',
+    'sequence': 0,
+    'description': """
+    Used to set PriceList with subscription inside Sale order
+    """,
+    'author': "Reliution",
+    'website': "https://www.reliution.com",
+    'depends': [
+        'base',
+        'sale_temporal'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
